9b69c659b9e0783482e53d09041f8a107f2bd4a8
Gouri Biswas <gouri.biswas@comprinno.net>
2026-09-16 12:43:01 +0530

    multi-level Inheritance

--- OOPS.java
+++ OOPS.java
@@ -9,9 +9,15 @@ class Triangle extends Shape {
         System.out.println(1/2*l*h);
     }
 }
+
+class EquilateralTriangle extends Triangle {
+     public void area(int l, int h) {
+        System.out.println(1/2*l*h);
+    }
+}
 //Inheritance  
 public class OOPS {
     public static void main (String[] args) {
-        
+
     }
 }
