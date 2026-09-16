b414693c03778fb0613ead73aa682f10031c1372
Gouri Biswas <gouri.biswas@comprinno.net>
2026-09-16 12:44:53 +0530

    Hirarchical Inheritance

--- OOPS.java
+++ OOPS.java
@@ -10,9 +10,11 @@ class Triangle extends Shape {
     }
 }
 
-class EquilateralTriangle extends Triangle {
-     public void area(int l, int h) {
-        System.out.println(1/2*l*h);
+
+
+class Circle extends Shape {
+    public void area(int r) {
+        System.out.println((3.14)*r*r);
     }
 }
 //Inheritance  
