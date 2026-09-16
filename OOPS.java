9f7743c02bfb1df7b4ec19c9a42fc8d4d2f3fd0e
Gouri Biswas <gouri.biswas@comprinno.net>
2026-09-16 15:04:29 +0530

    Multiple Inheritance in Java using Interfaces

--- OOPS.java
+++ OOPS.java
@@ -2,7 +2,11 @@ interface Animal {
     public void walk();
 }
 
-class Horse implements Animal {
+interface Herbivore {
+
+}
+
+class Horse implements Animal , Herbivore{
     public void walk() {
         System.out.println("walks on 4 legs");
     }
