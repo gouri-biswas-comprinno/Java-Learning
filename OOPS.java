09c0a4ee80e0f6e40d893f23a86a83274169c710
Gouri Biswas <gouri.biswas@comprinno.net>
2026-09-16 15:02:05 +0530

    Abstract i done properly using interfaces

--- OOPS.java
+++ OOPS.java
@@ -1,29 +1,16 @@
-//Just a concept a blueprint , cannot be created actually
-abstract class Animal {
-    abstract void walk();
-    //can have non-abstract method 
-    public void eat() {
-        System.out.println("Animal eats");
-    }
+interface Animal {
+    public void walk();
 }
 
-class Horse extends Animal {
+class Horse implements Animal {
     public void walk() {
-        System.out.println("Walks on 4 Legs");
+        System.out.println("walks on 4 legs");
     }
 }
-
-class Chicken extends Animal {
-    public void walk() {
-        System.out.println("Walks on 2 Legs");
-    }
-}
-
 public class OOPS {
     public static void main (String[] args) {
         Horse horse = new Horse();
         horse.walk();
-        horse.eat();
 
     }
 }
